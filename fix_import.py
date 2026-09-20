b0fe50b968c4071c5b285a2a732ad6f4b2ce3330
pi-primahome <pi.primahome@gmail.com>
2026-09-21 01:18:09 +0800

    cleanup: remove junk files, update gitignore

--- fix_import.py
+++
@@ -1,4 +0,0 @@
-with open('database/seeders/DatabaseSeeder.php', 'rb') as f:
-    for i, line in enumerate(f.read().split(b'\n'), 1):
-        if b'xAClass' in line or b' barang' in line or b' tanggal' in line:
-            print(i, line.hex())
