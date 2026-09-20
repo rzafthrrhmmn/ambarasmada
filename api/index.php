6680e07ad1cc6631daa10a41107bf218a8fb0a72
pi-primahome <pi.primahome@gmail.com>
2026-09-20 10:10:41 +0800

    update: pembaruan fitur pwa ambarasmada

--- api/index.php
+++ api/index.php
@@ -1,15 +1,28 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// Arahkan folder storage & cache sementara ke /tmp di lingkungan Vercel
+$storagePath = '/tmp/storage';
+
+if (! is_dir($storagePath)) {
+    mkdir($storagePath . '/framework/views', 0755, true);
+    mkdir($storagePath . '/framework/sessions', 0755, true);
+    mkdir($storagePath . '/framework/cache', 0755, true);
+    mkdir($storagePath . '/logs', 0755, true);
 }
 
-require __DIR__.'/../vendor/autoload.php';
+putenv('APP_STORAGE=' . $storagePath);
+putenv('VIEW_COMPILED_PATH=' . $storagePath . '/framework/views');
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($storagePath);
 
 $app->handleRequest(Request::capture());
