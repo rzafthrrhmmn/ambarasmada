caf7443d9251ace8c559c84810e4acf50738f79a
pi-primahome <pi.primahome@gmail.com>
2026-09-22 13:06:31 +0800

    fix: disable events cache to prevent facade root error

--- api/index.php
+++ api/index.php
@@ -38,8 +38,7 @@ $_SERVER = array_merge($_SERVER, $_ENV);
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 $app->useStoragePath($storagePath);
-$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
-$app->register(Illuminate\Events\EventServiceProvider::class);
+$app['config']->set('events.cache', false);
 $app->boot();
 
 $request = Request::capture();
